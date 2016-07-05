b078f1d9bb014f2db78b6baeacf31838adccf40f
craigminihan <craig@ripcordsoftware.com>
2016-07-05 23:55:01 +0100

    Added a bunch of test files to testlibstringintern * each file is run on a separate thread

--- src/testlibstringintern/main.cpp
+++ src/testlibstringintern/main.cpp
@@ -27,37 +27,104 @@
 #include <string>
 #include <cstring>
 #include <vector>
+#include <thread>
+#include <sstream>
+
+#include <string.h>
 
 #include "string_intern.h"
 
-int main() {
+std::vector<std::string> GetFileWords(const char* filename) {
+    std::vector<std::string> words;
     const auto delims = " \t\r\n\f,.\"\'()[]{}!:;&";
-    std::vector<rs::stringintern::StringReference> refs;
     
-    rs::stringintern::StringIntern intern;    
-
     std::fstream fs;
-    fs.open("rfc2616.txt", std::fstream::in);
+    fs.open(filename, std::fstream::in);
     
     char buffer[4096];
+    char* saveptr = nullptr;
     while (fs.getline(buffer, sizeof(buffer) - 1).good()) {
-        auto pStr = std::strtok(buffer, delims);
-        while (pStr != nullptr) {
-            refs.push_back(intern.Add(pStr));
-            pStr = std::strtok(nullptr, delims);
+        auto token = strtok_r(buffer, delims, &saveptr);
+        while (token != nullptr) {
+            words.emplace_back(token);
+            token = strtok_r(nullptr, delims, &saveptr);
         }
     }
     
     fs.close();
     
-    for (auto i = 0; i < refs.size(); ++i) {
-        const auto& ref = refs[i];
-        if (i > 0 && (i % 16) == 0) {
-            std::cout << intern.ToString(ref) << std::endl;
-        } else {
-            std::cout << intern.ToString(ref) << ' ';
-        }
+    return words;
+}
+
+std::vector<rs::stringintern::StringReference> InternWords(const std::vector<std::string>& words, rs::stringintern::StringIntern& intern) {
+    std::vector<rs::stringintern::StringReference> refs;
+    
+    for (auto word : words) {
+        auto ref = intern.Add(word);
+        refs.emplace_back(ref);
     }
     
-    std::cout << std::endl << "* pages = " << intern.GetPageCount() << ", entries = " << intern.GetEntryCount() << std::endl;
+    return refs;
+}
+
+void InternFile(const char* filename, rs::stringintern::StringIntern& intern) {   
+    auto words = GetFileWords(filename);
+    auto refs = InternWords(words, intern);
+    
+    if (words.size() != refs.size()) {
+        std::stringstream error;
+        error << "Count mismatch on '" << filename << "'";
+        throw error.str();
+    }
+    
+    for (auto i = 0; i < refs.size(); ++i) {
+        auto internedWord = intern.ToString(refs[i]);
+        if (words[i] != internedWord) {
+            std::stringstream error;
+            error << "String mismatch on '" << filename << "' at index " << i << ", expected '" << words[i] << "', got '" << internedWord << "'";
+            throw error.str();
+        }
+    }
+}
+
+int main() {
+    
+    std::vector<std::string> files{
+        R"(TestFiles/rfc/rfc2616.txt)", 
+        R"(TestFiles/gutenberg/11.txt)",
+        R"(TestFiles/gutenberg/74.txt)",
+        R"(TestFiles/gutenberg/76.txt)",
+        R"(TestFiles/gutenberg/98.txt)",
+        R"(TestFiles/gutenberg/158.txt)",
+        R"(TestFiles/gutenberg/308.txt)",
+        R"(TestFiles/gutenberg/345.txt)",
+        R"(TestFiles/gutenberg/1184-0.txt)",
+        R"(TestFiles/gutenberg/1342.txt)",
+        R"(TestFiles/gutenberg/1661.txt)",
+        R"(TestFiles/gutenberg/1952.txt)",
+        R"(TestFiles/gutenberg/2701.txt)",
+        R"(TestFiles/gutenberg/4300.txt)",
+        R"(TestFiles/gutenberg/5200.txt)",
+        R"(TestFiles/gutenberg/27827.txt)",
+        R"(TestFiles/gutenberg/30254.txt)",
+    };
+    
+    std::vector<std::thread> threads;
+    
+    rs::stringintern::StringIntern intern;
+
+    for (auto i = 0; i < files.size(); ++i) {
+        threads.emplace_back([&, i]() {
+            try {
+                InternFile(files[i].c_str(), intern);
+                std::cout << files[i] << ", pages = " << intern.GetPageCount() << ", entries = " << intern.GetEntryCount() << std::endl;
+            } catch (const std::string& error) {
+                std::cerr << "Error: " << error << std::endl;
+            }
+        });
+    }
+
+    for (auto i = 0; i < threads.size(); ++i) {
+        threads[i].join();
+    }
 }
