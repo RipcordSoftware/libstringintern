d77b2ce690e6f292f7b8aa8efaa140a1b981f3f1
craigminihan <craig@ripcordsoftware.com>
2016-09-23 14:35:51 +0100

    Added osx as a target * doesn't build anything yet

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -1,13 +1,18 @@
 mkdir -p .travis.temp
 pushd .travis.temp
 
-curl http://ftp.uk.debian.org/debian/pool/main/l/lcov/lcov_1.11.orig.tar.gz -O
-tar xfz lcov_1.11.orig.tar.gz
-mkdir -p lcov && make -C lcov-1.11/ install PREFIX=~/lcov
-export PATH=~/lcov/usr/bin:$PATH
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    curl http://ftp.uk.debian.org/debian/pool/main/l/lcov/lcov_1.11.orig.tar.gz -O
+    tar xfz lcov_1.11.orig.tar.gz
+    mkdir -p lcov && make -C lcov-1.11/ install PREFIX=~/lcov
+    export PATH=~/lcov/usr/bin:$PATH
 
-gem install coveralls-lcov
-pip install --user json_tools u-msgpack-python
-export PATH=~/.local/bin:$PATH
+    gem install coveralls-lcov
+    pip install --user json_tools u-msgpack-python
+    export PATH=~/.local/bin:$PATH
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    brew update
+    brew install make
+fi
 
 popd
--- .travis/script.sh
+++ .travis/script.sh
@@ -1,1 +1,8 @@
-make all CXX=g++-4.9 && make test CXX=g++-4.9 && ./coverage.sh gcov-4.9
+mkdir -p .travis.temp
+pushd .travis.temp
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    make all CXX=g++-4.9 && make test CXX=g++-4.9 && ./coverage.sh gcov-4.9
+fi
+
+popd
