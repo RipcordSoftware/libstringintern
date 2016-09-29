b9e59c7d6648fb40c5d5dc3cc222a2de98962b02
craigminihan <craig@ripcordsoftware.com>
2016-09-29 22:57:02 +0100

    Only submit coverage to coveralls from the Linux/GCC build [skip ci]

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,6 +1,5 @@
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    make CC=${_CC} CXX=${_CXX} all && make CC=${_CC} CXX=${_CXX} test && ./coverage.sh gcov-4.9
-elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    make CC=${_CC} CXX=${_CXX} all && make CC=${_CC} CXX=${_CXX} test
-fi
+make CC=${_CC} CXX=${_CXX} all && make CC=${_CC} CXX=${_CXX} test
 
+if [[ "${COV}" != "" ]]; then
+    ./coverage.sh ${_COV}
+fi
